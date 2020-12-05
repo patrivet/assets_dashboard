8c60db89f75139780ababec4802e622cdd412662
Pat Rivet <patrickmrivet@gmail.com>
2020-12-05 22:29:51 +0000

    Add sample graph to DeviceView Explicit 70 percent width given to DeviceView inside Dashboard container

--- src/components/GraphCPU/GraphCPU.js
+++ src/components/GraphCPU/GraphCPU.js
@@ -1,5 +1,35 @@
+import Chart from 'react-apexcharts';
+
 const GraphCPU = () => {
-  return <div className="graphCPU">GraphCPU</div>;
+  const state = {
+    options: {
+      chart: {
+        id: 'basic-bar',
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="graphCPU">
+        CPU
+        <Chart
+          options={state.options}
+          series={state.series}
+          type="bar"
+          height="90%"
+        />
+      </div>
+    </>
+  );
 };
 
 export default GraphCPU;
